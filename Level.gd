be789527c0b04487cf4610c7ac602201a46b2517
Jon Fincher <jfincher42@gmail.com>
2020-11-10 22:00:37 -0600

    Player initial impulse basics in place

--- Level.gd
+++ Level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var held_object = null
+var held_position
 
 func _ready():
 	# Set the player to not moving yet
@@ -11,9 +12,12 @@ func _on_Player_clicked(object):
 	if !held_object:
 		held_object = object
 		held_object.pickup()
+		held_position = get_global_mouse_position()
+		
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if held_object and !event.pressed:
-			held_object.drop(Input.get_last_mouse_speed())
+			var impulse = held_position - get_global_mouse_position()
+			held_object.drop(impulse)
 			held_object = null
